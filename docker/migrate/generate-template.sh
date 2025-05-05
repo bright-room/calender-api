b22bb4311147f54908300e578875e4a8015f11df
kukv <rio.by.kukv@gmail.com>
2025-05-06 02:54:52 +0900

    devcontainerでの開発に移行

--- docker/migrate/generate-template.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-migrate create -ext sql -dir /migrations -seq $@
--- docker/migrate/migration.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-migrate \
-  -path "/migrations" \
-  -database "postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable" \
-  "$@"
